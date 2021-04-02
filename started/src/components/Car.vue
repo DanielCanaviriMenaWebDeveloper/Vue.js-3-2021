5ae1ddd77fe3d481c6a77b1e5f34fbf555b8de4a
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-04-01 23:44:53 -0400

    v-for, v-if

--- started/src/components/Car.vue
+++ started/src/components/Car.vue
@@ -3,11 +3,31 @@
         <h2>Componente Car</h2>
         <p>Marca: {{ brand }}</p>
         <p>Modelo: {{ model }}</p>
-        <p>Color: {{ color[1] }}</p>
+        <p>Color: {{ colors[1] }}</p>
         <p>Precio: {{ price }} $</p>
         <p>Potencia: {{ power }} CV</p>
         <p>{{ mensaje.title }}</p>
-        <p>{{ mensaje.text }}</p> 
+        <p>{{ mensaje.text }}</p>
+        <!-- Ejemplo del uso de la directiva v-for --> 
+        <ul>Lista de Colores:
+            <li v-for="(color, index) in colors" :key="index">
+                {{ index }} : {{ color }}
+            </li>
+        </ul>
+        <!-- <ul>Lista de Potencias:
+            <li v-for="(potencia, index) in potencias" :key="index">
+                <template v-if="potencia<=120">{{ potencia }}</template>
+            </li>
+        </ul> -->
+        <ul>
+            <template v-for="(potencia, index) in potencias" :key="index">
+                <li v-if="potencia<300">{{potencia}}</li>
+            </template>
+        </ul>
+
+        <ul v-for="(tecnologia, index) in tecnologias" :key="index">
+            <li>{{ tecnologia }}</li>
+        </ul>
         <Test />
     </div>
 </template>
@@ -15,6 +35,7 @@
 <script>
 import Test from './Test';
 export default {
+    /* Option API */
     /* data() {
         return {
             brand: "Toyota",
@@ -28,19 +49,21 @@ export default {
             },
         }
     }, */
+    /* Composition API */
     setup() {
         const brand = "Nissan";
         const model = 2020;
-        const color = ['Negro', 'Rojo', 'Verde'];
+        const colors = ['Negro', 'Rojo', 'Verde', 'Azul', 'Morado', 'Rosado'];
         const price = 6000;
         const power = 340;
         const mensaje = { 
             title: 'Titulo del mensaje',
             text: 'Texto del mensaje'
         };
-
+        const potencias = [60, 80, 120, 160, 200, 280, 300, 390, 540, 500];
+        const tecnologias = ['JS', 'CSS', 'HTML', 'JQuery', 'React', 'PHP', 'Vue.js', 'Angular'];
         return {
-            brand, model, color, price, power, mensaje
+            brand, model, colors, price, power, mensaje, potencias, tecnologias
         };
     },
     components: {
